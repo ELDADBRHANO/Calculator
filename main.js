054af6ee3c8f0e565dfa6e754c2c5177cd46e2dc
ELDADBRHANO <ebrhano@gmail.com>
2022-06-16 20:23:40 +0300

    commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function calculate(num) {
+  input_screen.value+=num.value;
+  switch (+num) {
+    case equal_even:
+      num+num
+      break;
+    case "+":
+      break;
+    case "-":
+      break;
+    case "/":
+      break;
+    default:
+      break;
+  }
+}
