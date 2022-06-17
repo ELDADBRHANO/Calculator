f0174cf2b5fc575831b1f940a58b3fc7cc234ae9
ELDADBRHANO <ebrhano@gmail.com>
2022-06-18 00:28:24 +0300

    commit

--- main.js
+++ main.js
@@ -2,9 +2,19 @@ function calculate(num) {
 input_screen.value+=num.value;
 }
 
+let arrayNumbers=[];
+let ArrayOperators=[];
+let numToCalculate="";
   for(let i=0; i < input_screen.value.length; i++){
-    let arrayNumbers=[];
-    let ArrayOperators=[];
-    let numToCalculate="";
     numToCalculate+=input_screen[i]
+    switch (numToCalculate) {
+      case dual:
+        arrayNumbers.push(input_screen.value)
+        console.log(arrayNumbers);
+        break;
+    
+      default:
+        break;
+    }
+   
   }
