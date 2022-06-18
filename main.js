b622d0460c69e55f30c2dcd3d7eea09738567dfc
ELDADBRHANO <ebrhano@gmail.com>
2022-06-18 16:33:16 +0300

    commit

--- main.js
+++ main.js
@@ -21,26 +21,26 @@
 
 
 
-let btns = Array.from(document.getElementsByClassName("btn"));
-btns.map((btn) => {
-  btn.addEventListener("click", (event) => {
-    switch (event.target.innerText) {
-      case "AC":
-        input_screen.innerText = "";
-        break;
-      case "→":
-        if (input_screen.innerText) {
-          input_screen.innerText = input_screen.innerText.slice(0, -1);
-        }
-        break;
-        case "=":
-          input_screen.innerText= eval(input_screen.innerText);
-          break;
-      default:
-        input_screen.innerText += event.target.innerText;
-    }
-  });
-});
+// let btns = Array.from(document.getElementsByClassName("btn"));
+// btns.map((btn) => {
+//   btn.addEventListener("click", (event) => {
+//     switch (event.target.innerText) {
+//       case "AC":
+//         input_screen.innerText = "";
+//         break;
+//       case "→":
+//         if (input_screen.innerText) {
+//           input_screen.innerText = input_screen.innerText.slice(0, -1);
+//         }
+//         break;
+//         case "=":
+//           input_screen.innerText= eval(input_screen.innerText);
+//           break;
+//       default:
+//         input_screen.innerText += event.target.innerText;
+//     }
+//   });
+// });
 
 
 // eval is dangeros for app with real users!!!!
